c84fc7ecd88fe75c226cca35f067309e7757d495
Mariana Cruz <marianacruz23@aragon.unam.mx>
2026-05-24 19:36:30 -0600

    Agregar ruta principal

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+    res.send('Bienvenido a mi API Hola Mundo');
+});
+
 app.get('/hola', (req, res) => {
     res.json({
         mensaje: "Hola Mundo"
